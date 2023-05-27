c41f71919056586cc28dcf224518e7f547e6181b
Ana Dìaz <anm.diaz@alumnos.duoc.cl>
2023-05-26 21:44:58 -0400

    first commit

---
+++ src/main/java/cl/duoc/MsProductoBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductoBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductoBffApplication.class, args);
+	}
+
+}
